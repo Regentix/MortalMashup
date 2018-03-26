f586cafe19234d554bd0742fd81a104d0f03146b
DaanKDG <daan.boschmans@student.kdg.be>
2018-03-26 13:50:33 +0200

    Basic Phaser files

---
+++ main.js
@@ -0,0 +1,23 @@
+var game;
+
+function startGame() {
+	game = new Phaser.Game(320, 480, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
+	console.log("Game created!");
+}
+
+
+
+function preload(){
+  
+}
+
+function create() {
+
+
+}
+function update(){
+
+    
+}
+
+
