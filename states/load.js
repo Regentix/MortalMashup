093f5618208b51a7fa214b1241aba8c6accc6219
Indy Bosschem <32159085+IndyBoss@users.noreply.github.com>
2018-03-28 11:21:57 +0200

    overlay instead of buttons

--- states/load.js
+++ states/load.js
@@ -13,9 +13,9 @@ var loadState = {
         game.load.image("pause", "./assets/pause.png");
         game.load.image("resume", "./assets/resume.png");
         game.load.image("bullet", "./assets/bullet.png");
-        game.load.image("fire_L", "./assets/buttons/fire_L.png");
-        game.load.image("fire_R", "./assets/buttons/fire_R.png");
-        game.load.image("jump", "./assets/buttons/jump.png");
+        game.load.image("fire_L", "./assets/overlay/fire_L.png");
+        game.load.image("fire_R", "./assets/overlay/fire_R.png");
+        game.load.image("jump", "./assets/overlay/jump.png");
         game.load.image("floor", "./assets/world/floor.png");
         game.load.image("landscape", "./assets/world/landscape.png");
         game.load.spritesheet('player', './assets/player/Player_sheetBounds.png', 40, 60);
